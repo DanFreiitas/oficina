239d0e3864a6697acbd2a5281bcd3886eba9c336
Daniel Luiz <cross.adm@gmail.com>
2023-05-15 23:17:24 -0300

    Criação das telas Novo serviço, Listar serviço, Novo cliente, Listar cliente. Todas sendo chamadas como internalFrame de TelaPrincipal, que é chamada pela classe principal Oficina

--- src/main/java/com/mycompany/oficina/Oficina.java
+++ src/main/java/com/mycompany/oficina/Oficina.java
@@ -6,17 +6,8 @@ public class Oficina {
 
     public static void main(String[] args) {
 
-        Connexao conexao = new Connexao();
-
-        // Chama o método de conexão nesse objeto
-        Connection conn = conexao.conectaDB();
-
-        // Verifica se a conexão foi estabelecida
-        if (conn != null) {
-            System.out.println("Conexão estabelecida com sucesso!");
-        } else {
-            System.out.println("Falha ao conectar ao banco de dados.");
-        }
+        TelaPrincipal tp = new TelaPrincipal();
+        tp.setVisible(true);
 
     }
 }
