f0efeb94eccdb7634ae533a8ee60c1b4c2bf0c32
Daniel Luiz <cross.adm@gmail.com>
2023-05-15 22:01:37 -0300

    Modificação das pastas para uma pasta única e criação do arquivo de conexão

--- src/main/java/com/mycompany/oficina/Oficina.java
+++ src/main/java/com/mycompany/oficina/Oficina.java
@@ -1,16 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.oficina;
 
-/**
- *
- * @author Daniel
- */
+import java.sql.Connection;
+
 public class Oficina {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Connexao conexao = new Connexao();
+
+        // Chama o método de conexão nesse objeto
+        Connection conn = conexao.conectaDB();
+
+        // Verifica se a conexão foi estabelecida
+        if (conn != null) {
+            System.out.println("Conexão estabelecida com sucesso!");
+        } else {
+            System.out.println("Falha ao conectar ao banco de dados.");
+        }
+
     }
 }
